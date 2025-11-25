8252361651fd76569545a5ef4d1d744dc66694d0
asib11 <ahmedasib1@gmail.com>
2025-11-25 17:17:36 +0600

    feat: variable and type

---
+++ variable-and-type/variable.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	var a int = 10
+	var b string = "Golang"
+	var c float64 = 3.14
+	var d bool = true
+
+	// x := 20 // Short variable declaration
+
+	fmt.Println("Integer:", a)
+	fmt.Println("String:", b)
+	fmt.Println("Float:", c)
+	fmt.Println("Boolean:", d)
+}
