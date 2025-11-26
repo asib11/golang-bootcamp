f9b653123b2ee9f0ec539ce485011a344519b404
asib11 <ahmedasib1@gmail.com>
2025-11-26 09:59:15 +0600

    feat #1 : switch case

--- condition/condition.go
+++ condition/condition.go
@@ -3,13 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	age := 20
+	// age := 20
 
-	if age <18 {
-		fmt.Println("Minor")
-	} else if age >=18 && age <65 {
-		fmt.Println("Adult")
-	} else {
-		fmt.Println("Senior Citizen")
+	// if age <18 {
+	// 	fmt.Println("Minor")
+	// } else if age >=18 && age <65 {
+	// 	fmt.Println("Adult")
+	// } else {
+	// 	fmt.Println("Senior Citizen")
+	// }
+
+	a := 3
+	switch a {
+	case 1:
+		fmt.Println("One")
+	case 2, 3 :
+		fmt.Println("Two or Three")
+	default:
+		fmt.Println("Other Number")
 	}
 }
