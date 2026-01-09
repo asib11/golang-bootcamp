c432503361e8167d29414e0016be2bce054221f0
asib11 <ahmedasib1@gmail.com>
2026-01-09 11:11:13 +0600

    feat #15 : make slice function and len, cap;

--- slice/slice.go
+++ slice/slice.go
@@ -16,10 +16,32 @@ func main() {
 	fmt.Println(len(S))
 	fmt.Println(cap(S))
 
+	// slice from a slice
 	S1 := S[1:2] // Length = 2-1=1, Capacity = 5-1=4
 	fmt.Println(S1)
 	fmt.Println(len(S1))
 	fmt.Println(cap(S1))
+
+	// 2. slice literal
+	sliceLiteral := []int{10, 20, 30, 40, 50}
+	fmt.Println(sliceLiteral)
+	fmt.Println(len(sliceLiteral))
+	fmt.Println(cap(sliceLiteral))
+
+	// 4. function slice (make function)
+	scliceMake := make([]int, 3)
+	fmt.Println(scliceMake)
+	fmt.Println(len(scliceMake))
+	fmt.Println(cap(scliceMake))
+
+	// 5. make function with len and cap
+	s3 := make([]int, 2, 5)
+	fmt.Println(s3)
+	fmt.Println(len(s3))
+	fmt.Println(cap(s3))
+	s3[1] = 100
+	fmt.Println(s3)
+	
 }
 
 
@@ -31,6 +53,8 @@ func main() {
 * 1. slice from an existing array
 * 2. slice literal
 * 3. slice from a slice
+* 4. function slice (make function)
+* 5. make function with len and cap
 
 OUTPUT:
 [this is a very simple array]
